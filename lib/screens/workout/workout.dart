5ca4096c312e20b21a71b1e481bab70cce154140
suttipongJJ <62310072@go.buu.ac.th>
2022-03-11 23:20:10 +0700

    2

--- lib/screens/workout/workout.dart
+++ lib/screens/workout/workout.dart
@@ -1,28 +1,5 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  Widget build(BuildContext context) => Scaffold(
-        body: CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          slivers: [
-            buildAppBar(context),
-          ],
-        ),
-      );
-
-  SliverAppBar buildAppBar(BuildContext context) => SliverAppBar(
-        expandedHeight: MediaQuery.of(context).size.height * 0.5,
-        title: Text('Statistics'),
-        leading: Icon(Icons.menu),
-        actions: [
-          Icon(Icons.person, size: 28),
-          SizedBox(
-            width: 12,
-          )
-        ],
-      );
-}
-/*import 'package:flutter/material.dart';
 class Workout extends StatefulWidget {
   const Workout({Key? key}) : super(key: key);
 
@@ -34,14 +11,12 @@ class _WorkoutState extends State<Workout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Text("Your Workout"),
         centerTitle: true,
       ),
-      body: Center(
-          child: Text('Workout screen')
-      ),
+      body: Center(child: Text('Workout screen')),
     );
   }
-}*/
+}
