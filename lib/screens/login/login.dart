8be99fbea38404c142f05c9ab9c0ba0c18c089a8
Mujalin <59908391+Mujalin@users.noreply.github.com>
2022-03-12 00:22:05 +0700

    add login

---
+++ lib/screens/login/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class loginreg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Image.network(
+              'https://st2.depositphotos.com/4366957/6625/i/600/depositphotos_66253287-stock-photo-muscular-bodybuilder-guy-close-up.jpg'
+            ),
+          ]
+        ),
+      )
+    );
+  }
+}
