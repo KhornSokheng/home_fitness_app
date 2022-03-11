f3dd73fa8d969b3e513dcd358b9318af1b381e26
suttipongJJ <62310072@go.buu.ac.th>
2022-03-11 22:55:29 +0700

    1

--- lib/screens/workout/page.dart
+++ lib/screens/workout/page.dart
@@ -22,26 +22,3 @@ class HomePage extends StatelessWidget {
         ],
       );
 }
-/*import 'package:flutter/material.dart';
-class Workout extends StatefulWidget {
-  const Workout({Key? key}) : super(key: key);
-
-  @override
-  _WorkoutState createState() => _WorkoutState();
-}
-
-class _WorkoutState extends State<Workout> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey,
-      appBar: AppBar(
-        title: Text("Your Workout"),
-        centerTitle: true,
-      ),
-      body: Center(
-          child: Text('Workout screen')
-      ),
-    );
-  }
-}*/
