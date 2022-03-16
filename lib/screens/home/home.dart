a0672ee727d12ef56b0f46096163b63f5cc50d51
suttipongJJ <62310072@go.buu.ac.th>
2022-03-16 11:40:46 +0700

    homepate_fonts

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -14,11 +14,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-
     User user = Provider.of<UserProvider>(context).user;
 
     return Scaffold(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.white,
       extendBody: true,
       appBar: AppBar(
         title: Text("Home"),
@@ -27,32 +26,30 @@ class _HomeState extends State<Home> {
       body: Center(
         child: Column(
           children: [
-
             Text(
-              'Welcome ${user.first_name+' '+user.last_name}',
+              'Welcome ${user.first_name + ' ' + user.last_name}',
               style: TextStyle(
                 color: Colors.orangeAccent,
                 fontSize: 60,
-
+                fontFamily: 'RussoOne',
               ),
             ),
-
-            Image.network(
-                'https://st2.depositphotos.com/4366957/6625/i/600/depositphotos_66253287-stock-photo-muscular-bodybuilder-guy-close-up.jpg'
-
+            Image.asset(
+              'assets/Fitnss_logo.png',
+              width: 600,
+              height: 600,
             ),
-
+            /* Image.network(
+                'https://st2.depositphotos.com/4366957/6625/i/600/depositphotos_66253287-stock-photo-muscular-bodybuilder-guy-close-up.jpg'),*/
             Expanded(
               child: Container(
                 height: 300,
                 color: Colors.black12,
               ),
             )
-
           ],
         ),
       ),
-
     );
   }
 }
