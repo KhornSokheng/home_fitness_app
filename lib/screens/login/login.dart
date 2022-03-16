ab4286c3783ea5540b8cbee157b36cb4f9d62d43
Lapasrada <59908441+Lapasrada@users.noreply.github.com>
2022-03-16 13:30:37 +0700

    mmmm

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -8,7 +8,7 @@ class loginreg extends StatefulWidget {
 }
 
 class _loginregState extends State<loginreg> {
- final formKey = GlobalKey<FormState>();
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,55 +17,54 @@ class _loginregState extends State<loginreg> {
           key: formKey,
           child: SingleChildScrollView(
             child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text("E-mail : ",style: TextStyle(fontSize: 20)),
-              TextFormField(),
-              SizedBox(height: 15,),
-              Text("Password : ",style: TextStyle(fontSize: 20)),
-              TextFormField(
-                obscureText: true,
-              ),
-               SizedBox(
-                 height: 15,),
-              SizedBox(
-                child: ElevatedButton(child: Text("Login"),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
-                  builder:(context){
-                    return Launcher();
-                  })
-                  );
-                
-                },
-                ),
-              ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Don't have an Account ?",
-                style:
-                TextStyle(fontWeight:FontWeight.bold,fontSize: 10)),
-              SizedBox(
-                child: ElevatedButton(child: Text("Login",style: TextStyle(fontSize: 5)),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
-                  builder:(context){
-                    return reg();
-                  })
-                  );
-                },
+                Text("E-mail : ", style: TextStyle(fontSize: 20)),
+                TextFormField(),
+                SizedBox(
+                  height: 15,
                 ),
-              ),
+                Text("Password : ", style: TextStyle(fontSize: 20)),
+                TextFormField(
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  child: ElevatedButton(
+                    child: Text("Login"),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return Launcher();
+                      }));
+                    },
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an Account ?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 10)),
+                    SizedBox(
+                      child: ElevatedButton(
+                        child: Text("Login", style: TextStyle(fontSize: 5)),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return reg();
+                          }));
+                        },
+                      ),
+                    ),
+                  ],
+                )
               ],
-            )
-            ],
-         
             ),
-            ),
-            ),
-            
-
+          ),
+        ),
       ),
     );
   }
