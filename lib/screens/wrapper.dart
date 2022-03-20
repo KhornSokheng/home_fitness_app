4c07ce97357c0958c743427bbe516f2f7ca407ce
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2022-03-21 02:25:10 +0700

    add stream builder, push to Launcher() when logged in

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:home_fitness/screens/achievement/achievement.dart';
 import 'package:home_fitness/screens/admin/admin_report.dart';
@@ -18,48 +19,67 @@ class Wrapper extends StatelessWidget {
 
 class LoginOrMenu extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return loginreg();
-                }));
-              },
-              child: Text('Login Test')),
-          SizedBox(
-            width: 100,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                // MaterialPageRoute(builder: (BuildContext context) {
-                //   return Launcher();
-                // });
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return Launcher();
-                }));
-              },
-              child: Text('Go to Menu')),
-          SizedBox(
-            width: 100,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                // MaterialPageRoute(builder: (BuildContext context) {
-                //   return Launcher();
-                // });
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (BuildContext context) {
-                  return AdminReport();
-                }));
-              },
-              child: Text('Log in as Admin'))
-        ],
-      ),
-    );
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData) {
+            Navigator.push(context,
+            MaterialPageRoute(builder: (BuildContext context) {
+            return Launcher();}));
+            // return Launcher();
+            // return LoggedInWidget();
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Something Went Wrong!'));
+          } else {
+            // return SignUpWidget();
+          }
+
+          return Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return loginreg();
+                      }));
+                    },
+                    child: Text('Login Test')),
+                SizedBox(
+                  width: 100,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      // MaterialPageRoute(builder: (BuildContext context) {
+                      //   return Launcher();
+                      // });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return Launcher();
+                      }));
+                    },
+                    child: Text('Go to Menu')),
+                SizedBox(
+                  width: 100,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      // MaterialPageRoute(builder: (BuildContext context) {
+                      //   return Launcher();
+                      // });
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return AdminReport();
+                      }));
+                    },
+                    child: Text('Log in as Admin'))
+              ],
+            ),
+          );
+        });
   }
 }
